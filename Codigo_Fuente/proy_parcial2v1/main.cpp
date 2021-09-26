4c1f5763f252a4cc3047274961a782ff8dff2957
JuanDiegoCaMo <juan.cabrera@udea.edu.co>
2021-09-26 18:35:51 -0500

    func de subm lista + pruebas con sobrem

--- Codigo_Fuente/proy_parcial2v1/main.cpp
+++ Codigo_Fuente/proy_parcial2v1/main.cpp
@@ -23,15 +23,5 @@ int main()
     getline(cin,filename);
     QImage im(filename.c_str());
     pix_mat matriz_pixeles(im);
-    unsigned int pixelX = 200;
-    unsigned int pixelY = 200;
-    cout << "Intensidad de rojo del pixel seleccionado: " << im.pixelColor(pixelX, pixelY).red() << endl;
-    cout << "Intensidad de verde del pixel seleccionado: " << im.pixelColor(pixelX, pixelY).green() << endl;
-    cout << "Intensidad de azul del pixel seleccionado: " << im.pixelColor(pixelX, pixelY).blue() << endl;
-    /*for(int indx = 0; indx < im.width(); ++indx){
-        for(int indy = 0; indy < im.height(); ++indy){
-            cout << indx << ", " << indy << " = " << im.pixelColor(indx,indy).red() << endl;
-        }
-    }*/
     return 0;
 }
--- Codigo_Fuente/proy_parcial2v1/pix_mat.h
+++ Codigo_Fuente/proy_parcial2v1/pix_mat.h
@@ -10,8 +10,10 @@ class pix_mat
 {
 public:
     pix_mat(QImage image_dir);
+    void assignToPix(QImage image_dir);
+    void remuestreo(QImage image_dir, int *auxX, int *auxY);
 private:
-    vector<short int[3][8][8]> pixel;
+    vector<short int> pixel;
     short int **mat;
     int *auxX, *auxY;
 };
